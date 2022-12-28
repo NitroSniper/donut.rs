8b5e60f8caa6e532794cb7bfb4cf29b9e8beb3b1
NitroSniper <mahiemiah35@gmail.com>
2022-12-28 20:50:28 +0000

    Finished creating the program

--- src/main.rs
+++ src/main.rs
@@ -20,22 +20,25 @@ fn main() {
     const X_OFFSET: f64 = 2.0;
     const RADIUS: f64 = 1.0;
     
-
-    // TaSK: Generate points in a circle.
-    
     // the step constant going around the circumference. 
     const CIRCLE_STEPS: f64 = 0.02;
     const TORUS_STEPS: f64 = 0.02;
     
     // generate the angles
-    let a: f64 = 0.0;
-    let b: f64 = 0.0;
-    const SCREEN_WIDTH: f64 = 100.0;
-    const SCREEN_HEIGHT: f64 = 100.0;
+    let mut a: f64 = 0.0;
+    let mut b: f64 = 0.0;
+    const SCREEN_WIDTH: f64 = 40.0;
+    const SCREEN_HEIGHT: f64 = 40.0;
     // K Constants
     // Calculate K1 with the screen size which is the terminal
     let k_2: f64 = 5.0;
     let k_1: f64 = SCREEN_WIDTH*k_2*3.0/(8.0*(RADIUS+X_OFFSET));
+
+    loop {
+        a += 0.07;
+        b += 0.03;
+    let mut screen_output = [[' '; SCREEN_WIDTH as usize]; SCREEN_HEIGHT as usize];
+    let mut z_buffer = [[0.0; SCREEN_WIDTH as usize]; SCREEN_HEIGHT as usize];
 
     // rotate the torus around y axis
     // cargo clippy suggests TaU in std instead of 6.28
@@ -58,8 +61,6 @@ fn main() {
             let sin_b = b.sin();
             let cos_b = b.cos();
 
-
-
             let circle_point = Point2D {
                 x: cos_theta*RADIUS + X_OFFSET, 
                 y: sin_theta*RADIUS
@@ -75,27 +76,46 @@ fn main() {
             
             // store inverse of z+k_2 for peformance
             let inverse_z = 1.0/(transformed_point.z+k_2);
+            
             // since we are writing this on a terminal which doesn't consider the origin as the
             // middle. we have to offset the drawing to match our calculation.
+            
             let screen_point = Point2D {
                 x: SCREEN_WIDTH/2.0 + k_1*inverse_z*transformed_point.x,
                 y: SCREEN_HEIGHT/2.0 - k_1*inverse_z*transformed_point.y, // - because y goes
                                                                           // downwards.
             };
+            
             // now time to create lighting which follows the same matrix calculation cause a circle
             // at the orgin mirrors the surface normal. 
             // we will also do a dot product on it to calcluate the number. 
             let lighting = {
-                cos_phi*cos_theta*sin_a-
+                cos_phi*cos_theta*sin_b-
                 cos_a*cos_theta*sin_phi-
                 sin_a*sin_theta+
                 cos_b*(cos_a*sin_theta-cos_theta*sin_a*sin_phi)
             };
-
-
-
             
-        
+            // screen_point needs to be in a usize format
+            let x_pixel = screen_point.x as usize;
+            let y_pixel = screen_point.y as usize;
+
+            // if it is less than 0 then it is dark
+            if lighting > 0.0 && z_buffer[y_pixel][x_pixel] < inverse_z {
+                z_buffer[y_pixel][x_pixel] = inverse_z;
+                let luminance_index = (lighting*8.0) as usize;
+                screen_output[y_pixel][x_pixel] = ".,-~:;=!*#$@"
+                    .chars().nth(luminance_index)
+                    .expect("luminance_index is not between 0-11")
+            }
         }
     }
+    // now draw it onto the terminal 
+    print!("\x1b[H");
+    for row in screen_output {
+        for pixel in row {
+            print!("{}", pixel);
+        }
+        println!();
+    }}
 }
